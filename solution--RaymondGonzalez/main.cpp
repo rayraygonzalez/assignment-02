7d80724e7a03cfd029be3b46783819513bee9ade
rayraygonzalez <rayraygking@csu.fullerton.edu>
2016-02-27 21:01:05 -0800

    assignment 2

---
+++ solution--RaymondGonzalez/main.cpp
@@ -0,0 +1,11 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Raymond Gonzalez <rayraygking@csu.fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+#include <iostream>
+using std::cout;
+int main()
+{
+	cout << "Raymond Gonzalez";
+}
